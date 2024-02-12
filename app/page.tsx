f71359a1a4af497caf2208dd2a3dbc72647657ac
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-02-12 12:43:25 -0500

    ui: reformat home page with logo, text and format to fullscreen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 "use client";
-import { Box, CircularProgress, Container, Stack } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  CircularProgress,
+  Container,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import Question from "./components/Question";
 import Result from "./components/Result";
+import Image from "next/image";
+import AiLogo from "../public/Ai-Logo.svg";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2b2b2b',
+    },
+  },
+});
 type SourceResponse = {
   source: String;
   score: Number;
@@ -23,12 +42,12 @@ export default function Home() {
 
   return (
     <main>
-      <Container maxWidth="lg">
+          <ThemeProvider theme={theme}>
+
+      <div>
         <Box
           sx={{
             bgcolor: "#1e1e1e",
-            height: "90vh",
-            padding: "20px",
           }}
         >
           <Stack direction="column" spacing={2}>
@@ -39,27 +58,64 @@ export default function Home() {
                 flexDirection: "column",
                 height: 700,
                 overflow: "hidden",
-                overflowY: "scroll",
                 bgcolor: "#2b2b2b",
-                padding: "20px",
               }}
             >
-              {results && (
-                <>
+              <AppBar
+                sx={{
+                  backgroundColor: "#000810",
+                }}
+              >
+                <Toolbar>
+                <Image
+                        src={AiLogo}
+                        width={30}
+                        height={30}
+                        alt="Ai Small Logo"
+                      />
+                <Typography sx={{ml:2}}>AI Search Genius</Typography>
+                </Toolbar>
+              </AppBar>
+              {results ? (
+                <Container maxWidth="lg">
                   <Result question={question} results={results} />
+                </Container>
+              ) : (
+                <>
+                  <Container
+                    sx={{ textAlign: "center", color: "#FFF", pt: 32 }}
+                    maxWidth="sm"
+                  >
+                    <Box pb={2}>
+                      <Image
+                        src={AiLogo}
+                        width={150}
+                        height={100}
+                        alt="Ai Logo"
+                      />
+                    </Box>
+                    <Typography gutterBottom variant="h5" component="h1">
+                      <strong>Where Conversations Get Resolved</strong>
+                    </Typography>
+                    <Typography>
+                      Expert guidance on all things related to Health
+                      Information Technology. From understanding healthcare
+                      systems to navigating digital solutions.
+                    </Typography>
+                  </Container>
                 </>
               )}
+              <Question
+                setResults={setResults}
+                setQuestion={setQuestion}
+                setIsLoading={setIsLoading}
+                question={question}
+              />
             </Box>
-
-            <Question
-              setResults={setResults}
-              setQuestion={setQuestion}
-              setIsLoading={setIsLoading}
-              question={question}
-            />
           </Stack>
         </Box>
-      </Container>
+      </div>
+      </ThemeProvider>
     </main>
   );
 }
